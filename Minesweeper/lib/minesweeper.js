2f32528cb89559fe246892563853aa3eb406fa4e
DzTheRage <32145716+DzTheRage@users.noreply.github.com>
2017-10-09 21:34:58 -0500

    MineSweeper JS

---
+++ Minesweeper/lib/minesweeper.js
@@ -0,0 +1,11 @@
+let blankLine = '  |   |  ';
+let guessLine = '1 |   |  ';
+let bombLine = '  | B |  ';
+console.log("This is what an empty board would look like:");
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+console.log("This is what a board with a guess and a bomb on it would look like:");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
