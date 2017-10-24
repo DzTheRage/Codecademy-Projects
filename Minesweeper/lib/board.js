3b3cd9188fa18174471c1e23b758025b5ba4ec28
DzTheRage <32145716+DzTheRage@users.noreply.github.com>
2017-10-24 08:41:10 -0500

    Minesweeper Project
    Added classes and interactivity in console.

--- Minesweeper/lib/board.js
+++ Minesweeper/lib/board.js
@@ -1,21 +1,4 @@
-class Game {
-    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-        this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-    }
-    playMove(rowIndex, columnIndex) {
-        this._board.flipTile(rowIndex, columnIndex);
-        if (this._board.playerBoard[rowIndex][columnIndex] == 'B') {
-            console.log('Game over!');
-            this._board.print();
-        } else if (!this._board.hasSafeTiles) {
-            console.log("Congratulations You've won");
-        } else {
-            console.log("Current Board:");
-            this._board.print();
-        }
-    }
-}
-class Board {
+module.exports = class Board {
     constructor(numberOfRows, numberOfColumns, numberOfBombs) {
         this._numberOfBombs = numberOfBombs;
         /*this._numberOfRows = numberOfRows;
@@ -100,8 +83,4 @@ class Board {
         return board;
     }
 
-}
-
-const g = new Game(3, 3, 3);
-
-g.playMove(0, 0);
+};
--- Minesweeper/src/board.js
+++ Minesweeper/src/board.js
@@ -1,22 +1,5 @@
-class Game {
-    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-        this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-    }
-    playMove(rowIndex, columnIndex) {
-        this._board.flipTile(rowIndex, columnIndex);
-        if (this._board.playerBoard[rowIndex][columnIndex] == 'B') {
-            console.log('Game over!');
-            this._board.print();
-        } else if (!this._board.hasSafeTiles) {
-            console.log("Congratulations You've won");
-        } else {
-            console.log("Current Board:");
-            this._board.print();
-        }
-    }
-}
-class Board {
-    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+module.exports = class Board{
+    constructor(numberOfRows, numberOfColumns, numberOfBombs){
         this._numberOfBombs = numberOfBombs;
         /*this._numberOfRows = numberOfRows;
         this._numberOfColumns = numberOfColumns;*/
@@ -24,7 +7,7 @@ class Board {
         this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
         this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
     }
-    get playerBoard() {
+    get playerBoard(){
         return this._playerBoard;
     }
     flipTile(rowIndex, columnIndex) {
@@ -38,9 +21,9 @@ class Board {
             console.log("Bombs " + this.getNumberofNeighborBombs(rowIndex, columnIndex));
             this._playerBoard[rowIndex][columnIndex] = this.getNumberofNeighborBombs(rowIndex, columnIndex);
         }
-        this._numberOfTiles -= 1;
+        this._numberOfTiles -=1;
     }
-    getNumberofNeighborBombs(rowIndex, columnIndex) {
+    getNumberofNeighborBombs (rowIndex, columnIndex) {
         const neighborOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
         const numberOfRows = this._bombBoard.length;
 
@@ -58,14 +41,14 @@ class Board {
 
         return numberOfBombs;
     }
-    hasSafeTiles() {
-        return this._numberOfTiles != this._numberOfBombs;
+    hasSafeTiles(){
+        return (this._numberOfTiles != this._numberOfBombs);
     }
-    print() {
+    print(){
         console.log(this._playerBoard.map(row => row.join('|')).join('\n'));
     }
 
-    static generatePlayerBoard(numberOfRows, numberOfColumns) {
+    static generatePlayerBoard (numberOfRows, numberOfColumns) {
         let board = [];
         for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
             let row = [];
@@ -77,7 +60,7 @@ class Board {
         return board;
     }
 
-    static generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs) {
+    static generateBombBoard (numberOfRows, numberOfColumns, numberOfBombs) {
         let board = [];
         for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
             let row = [];
@@ -100,8 +83,4 @@ class Board {
         return board;
     }
 
-}
-
-const g = new Game(3, 3, 3);
-
-g.playMove(0, 0);
+};
