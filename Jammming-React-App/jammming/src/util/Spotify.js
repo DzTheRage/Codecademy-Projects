1163d064c95d991cfec01bb40fac927a2e59f443
DzTheRage <32145716+DzTheRage@users.noreply.github.com>
2017-11-15 16:32:45 -0600

    Surge Update
    Updated deployment to surge server at scottJAM.surge.sh

--- Jammming-React-App/jammming/src/util/Spotify.js
+++ Jammming-React-App/jammming/src/util/Spotify.js
@@ -1,7 +1,7 @@
 let userAccessToken = undefined;
 let userAccessTokenExpire = undefined;
 let clientID = '875cadc94aa5452b86650aa2b3270d52';
-let redirectURI = 'http://localhost:3000/';
+let redirectURI = 'http://scottJAM.surge.sh/';
 let spotifyURL = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientID}&redirect_uri=${redirectURI}`;
 
 const Spotify = {
